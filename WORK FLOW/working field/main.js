a8dfeaa02fec3bd6f79a40a1436fd3fe959dc4ad
PRAVEEN200307 <crouselearn@gmail.com>
2023-11-01 15:55:26 +0530

    console log,variable

---
+++ WORK FLOW/working field/main.js
@@ -0,0 +1,34 @@
+
+// alert("welcome to js  hello world");
+
+// console.log("Welcome to js developer");
+// console.log("Number :",12);
+// console.log("Number :",123433);
+// console.log(true);
+// console.log([55,77,88,99]);
+// console.table({fname:'praveen',age:18});
+// // 13:47
+// console.error("sample error ");
+// console.warn("This is the warning message");
+// console.clear();
+
+// ------------------------------------------------------
+
+// varible 
+    // var,let,const
+     
+    // var a=10;
+    // console.log(a);
+
+    // const a=45;
+    // console.log(a);
+    //  a=50;
+    //  console.log(a);
+    // object
+        // const student =  {'name':'praveen','age':'25'}
+        // console.log(student.name);
+
+        // student.name="sivaraman";
+        // console.log(student.name);
+
+// -----------------------------------------------------------

d749e2f2528f3d62ced299629495fbd05f518c51
PRAVEEN200307 <crouselearn@gmail.com>
2023-11-13 16:51:09 +0530

    label , Math function

--- WORK FLOW/working field/main.js
+++ WORK FLOW/working field/main.js
@@ -814,25 +814,27 @@
  */
 
 // Break Statement 
-//  for(let i=1;i<=10;i++){
-//    console.log(i);
-//    if(i==4){
-//      break;
-//    }
-//  }
+/* 
+  for(let i=1;i<=10;i++){
+    console.log(i);
+    if(i==4){
+      break;
+    }
+  }
+*/
 
 //continue statement --skaping perticular activity
-// for(let i=1;i<=10;i++){
- 
-//   if(i==4){
-//     continue;
-//   }
-//   console.log(i);
-// }
+/*
+  for(let i=1;i<=10;i++){
+  
+    if(i==4){
+      continue;
+    }
+    console.log(i);
+  }
 
-// for(let i=1;i<=10;i++){
-//   if(i%2==0){
-//     continue
-//   }
-//   console.log(i);
-// }
+*/
+
+//---------------------------------------------------------
+
+
---
+++ WORK FLOW/working field/main1.js
@@ -0,0 +1,57 @@
+/*
+    Label Block in JavaScript
+    Math Functions in JavaScript
+    String Functions in JavaScript
+    Function in JavaScript
+    Arbitrary Function in JavaScript
+    Function as Expression in JavaScript
+    ES6 Arrow function in JavaScript
+    Callback Functions in JavaScript
+    Block Scope and Function Scope in JavaScript
+*/
+
+// Label Block 
+    // startWidth
+    // Dont use this Only for knowledge
+    /*
+    let groups =[
+    ["Ram","Sam","Ravi"],
+    ["kumar","Tiya","Sundar"],
+    ["Rajesh","Sara","Rahul"],
+    ];
+
+    for(let group of groups){
+        inner:
+        for(let member of group){
+        //  console.log(member);
+        if(member.startsWith('R')){
+            console.log(member);
+            break inner;
+        }
+        }
+    } 
+*/
+
+// Math Functions
+
+/*
+ let val=Math.PI; //3.141592653589793
+ val=Math.E; //2.718281
+ c=Math.round(5.8);
+ c=Math.floor(56.90);///giving only rounded values
+ c=Math.ceil(56.7); //point values consider full number
+ c=Math.sqrt(5); //giving square rooot number
+ c=Math.abs(-8) //giving  negative number to positive number
+ c=Math.trunc(2.58); //giving only integer values
+ c=Math.pow(2,3); // Converting square values
+ c=Math.min(15,34,26,28,30);
+ c=Math.max(89,90,430,160);
+ c=Math.random();
+ c=Math.floor((Math.random()*50+1));
+ c=Math.sign(-98);
+ c=Math.sin(56);
+ c=Math.cos(43);
+ c=Math.log(10);
+ c=Math.log10(10);
+ console.log(c);
+*/

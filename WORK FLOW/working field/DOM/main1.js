943c7127483c1fba561c768bd4f5eb94128732ef
PRAVEEN200307 <crouselearn@gmail.com>
2023-12-09 15:21:20 +0530

    git window

--- WORK FLOW/working field/DOM/main1.js
+++ WORK FLOW/working field/DOM/main1.js
@@ -545,9 +545,31 @@ game.addEventListener("touchmove",function(e){
      5.window
       scroll
       resize
-      load
       haschange 
 
  */
+//scroll
+// window.addEventListener("scroll",()=>{
+//    console.log("Scrolled");
+   
+// });
+
+//resize
+
+// const container = document.querySelector(".container");
+
+// const observer=new ResizeObserver((items)=>{
+//   let containerwidth = Math.ceil(items[0].contentRect.width);
+
+//   if(containerwidth <400){
+//     items[0].target.style.backgroundColor="red";
+//   }
+//   else{
+//     items[0].target.style.backgroundColor="#212121";
+//   }
+// });
+
+// observer.observe(container);
 
 
+//haschange 

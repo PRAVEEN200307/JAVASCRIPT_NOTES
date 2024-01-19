467a7807999685fbf245675ca0415c6f5180213c
PRAVEEN200307 <crouselearn@gmail.com>
2024-01-19 16:53:58 +0530

    array of object using localStorage

--- WORK FLOW/working field/advance concept/array.js
+++ WORK FLOW/working field/advance concept/array.js
@@ -1,90 +1,127 @@
-//Store array
-
-
-const form = document.forms.todoApp
-const inputEl = form.elements.foodNames
-const submitBtn = document.querySelector('#submit-btn');
-const foodList = document.querySelector('#food-list');
+//Store array of Objects
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadstoredData();
+    const model = document.querySelector('.model');
+    const closeBtn = document.querySelector('.close');
+    const formEl = document.forms.dataForm;
 
-    form.addEventListener('submit', handlesubmit);
+    formEl.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const form = formEl.elements;
+        const fullName = form.nameInput.value.trim();
+        const age = parseInt(form.ageInput.value);
+        const gender = form.genderSelect.value;
 
-    const editBtn = document.querySelectorAll('#editBtn');
-    console.log(editBtn);
-    editBtn.forEach(btn => {
-        btn.addEventListener('click', handleEdit);
-    })
+        if (fullName !== '' && !isNaN(age) && gender !== '') {
+            const user = {
+                name: fullName,
+                age,
+                gender
+            }
+            addLocalStorage(user);
+            loadStoredData();
+            formEl.reset();
+        } else {
+            alert("Please fill all detail")
+        }
+    });
 
-    const deleteBtn = document.querySelectorAll('#delectBtn');
-    deleteBtn.forEach(value => {
-        value.addEventListener('click', handleDelete);
-    })
+    const formEdit = document.forms.editDataForm;
+    formEdit.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const formElement = document.forms.editDataForm.elements;
+        const index = formElement.editIndex.value
+        const fullName = formElement.editNameInput.value
+        const age = formElement.editAgeInput.value;
+        const gender = formElement.editGenderSelect.value;
+
+        if (fullName !== '' && !isNaN(age) && gender !== '') {
+            const data = JSON.parse(localStorage.getItem('myData'));
+            data[index].name = fullName;
+            data[index].age = age;
+            data[index].gender = gender;
+            localStorage.setItem('myData', JSON.stringify(data));
+            loadStoredData();
+            model.style.display = 'none';
+        }
+        else {
+            alert("Plese fill all detail");
+        }
+
+    });
+
+
+
+    function addLocalStorage(user) {
+        const storedData = JSON.parse(localStorage.getItem('myData')) || [];
+        storedData.push(user);
+        localStorage.setItem('myData', JSON.stringify(storedData));
+    }
+
+    loadStoredData();
+
+
+    //Function to close the model
+    closeBtn.addEventListener('click', () => {
+        model.style.display = 'none';
+    });
+
+    window.addEventListener('click', (e) => {
+        if (e.target === model) {
+            model.style.display = 'none';
+        }
+    });
+
+    function loadStoredData() {
+        const tableBody = document.querySelector('#dataList');
+        const storedData = JSON.parse(localStorage.getItem("myData")) || [];
+        tableBody.innerHTML = '';
+        storedData.forEach((data, index) => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+        <td>${data.name}</td>
+        <td>${data.age}</td>
+        <td>${data.gender}</td>
+        <td><button data-index=${index} id="editBtn">Edit</button></td>
+        <td><button  data-index=${index} id="deleteBtn">Delete</button></td>
+        `;
+            tableBody.appendChild(tr);
+        });
+
+
+        const editBtns = document.querySelectorAll("#editBtn");
+        editBtns.forEach(values => {
+            values.addEventListener('click', (e) => {
+                model.style.display = 'block';
+                const storedData = JSON.parse(localStorage.getItem('myData') || [])
+                const index = e.target.dataset.index
+                const userData = storedData[index];
+
+                const formElement = document.forms.editDataForm.elements;
+                formElement.editIndex.value = index;
+                formElement.editNameInput.value = userData.name;
+                formElement.editAgeInput.value = userData.age;
+                formElement.editGenderSelect.value = userData.gender;
+            })
+        });
+
+        const deleteBtns = document.querySelectorAll("#deleteBtn");
+        deleteBtns.forEach(values => {
+            values.addEventListener('click', (e) => {
+                if (confirm("Are you Sure to Delete ?")) {
+                    const index = e.target.dataset.index
+                    const storedData = JSON.parse(localStorage.getItem('myData') || [])
+                    storedData.splice(index, 1);
+                    localStorage.setItem('myData', JSON.stringify(storedData));
+                    loadStoredData();
+                }
+            })
+        });
+    };
 
 });
 
-function loadstoredData() {
-
-    foodList.innerHTML = '';
-    const productArrayList = JSON.parse(localStorage.getItem('getItems')) || [];
-
-    productArrayList.forEach((value, index) => {
-        const li = document.createElement('li');
-        li.innerHTML = `${value}
-                <div>
-                   <button id="editBtn" data-index=${index} >Edit</button>
-                   <button id="delectBtn" data-index=${index} >Delect</button>
-                 </div>`;
-
-        foodList.appendChild(li);
-    });
-}
-
-function addLocalStorage(data) {
-    const productArrayList = JSON.parse(localStorage.getItem('getItems')) || [];
-
-    productArrayList.push(data);
-    const productjson = JSON.stringify(productArrayList);
-    localStorage.setItem("getItems", productjson);
-}
-
-const handlesubmit = (e) => {
-    e.preventDefault();
-    const data = inputEl.value.trim();
-    if (data !== '') {
-        addLocalStorage(data);
-        loadstoredData();
-        inputEl.value = '';
-    } else {
-        alert('Please Enter The data');
-        inputEl.focus();
-    }
-}
-
-
-//handling edit button
-
-const handleEdit = (e) => {
-    const index = e.target.dataset.index
-    const productArrayList = JSON.parse(localStorage.getItem('getItems')) || [];
-    const editedName = prompt('Enter a your Name :', productArrayList[index]);
-
-    if (editedName !== '') {
-        productArrayList[index] = editedName;
-        localStorage.setItem('getItems', JSON.stringify(productArrayList));
-        loadstoredData();
-    }
-}
-
-//handling delect button
-const handleDelete = (e) => {
-   if(confirm('Are you sure to Delete')){
-    const index = e.target.dataset.index
-    const productArrayList = JSON.parse(localStorage.getItem('getItems')) || [];
-    productArrayList.splice(index, 1);
-
-    localStorage.setItem('getItems', JSON.stringify(productArrayList));
-    loadstoredData();
-   }
-}
+//Learn from the Project
+//isNaN() meathod
+//formEl.reset();

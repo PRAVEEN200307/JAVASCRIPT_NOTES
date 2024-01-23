838b5257bb247cd30cddd8c834d6c0b6af079984
PRAVEEN200307 <crouselearn@gmail.com>
2024-01-23 18:59:30 +0530

    set method

--- WORK FLOW/working field/advance concept/array.js
+++ WORK FLOW/working field/advance concept/array.js
@@ -54,4 +54,141 @@ async function getApi() {
 btnApi.addEventListener('click', getApi);
 ----------------------------------------------------------------------
 */
-//Set meathod
+//Set class
+
+const users = ["ram", "sam", "ram", "rajesh"];
+console.log(users);
+
+const setOb = new Set(users);
+setOb.add(10).add(80);
+setOb.delete(10)
+setOb.delete(80)
+
+//entries
+console.log(setOb);
+console.log(setOb.has('rajesh'));
+entries = setOb.entries(values => {
+    console.log(values);
+});
+console.log(entries.next().value);
+console.log(entries.next().value);
+console.log(entries.next().value);
+
+//keys
+const val = setOb.values();
+console.log(val.next().value);
+console.log(val.next().value);
+console.log(val.next().value);
+
+/**
+
+    ƒ add()
+    ƒ clear()
+    ƒ Set()
+    ƒ delete()
+    ƒ entries()
+    ƒ forEach()
+    ƒ has()
+    f keys()
+    ƒ values()
+
+*/
+
+//Spread
+//  const setArray  =[...setOb]
+//  console.log(setArray);
+
+
+
+//User define functions
+/**
+ * Subset
+ *  A = 1 ,2 , 3
+ *  B = 5 ,6 , 1, 2, 3
+ *  c = 1 ,3 , 4, 5
+ * union
+ * Intersection
+ * difference
+ */
+
+
+Set.prototype.subset = function (otherset) {
+    if (this.size > otherset.size) {
+        return false
+    } else {
+        for (var element of this) {
+            if (!otherset.has(element)) return false;
+        }
+        return true
+    }
+};
+
+const setA = new Set([1, 2, 3]);
+const setB = new Set([5, 6, 1, 2, 3, 4]);
+const setC = new Set([1, 3, 4, 5]);
+
+
+console.log(setB.subset(setA));
+console.log(setA.subset(setC));
+console.log(setC.subset(setB));
+
+
+Set.prototype.union = function (otherset) {
+    const unionset = new Set();
+    for (let element of this) {
+        unionset.add(element);
+    }
+    for (let element of otherset) {
+        unionset.add(element);
+    }
+    return unionset
+}
+
+console.log(setA.union(setB));
+console.log(setA.union(setB));
+console.log(setB.union(setC));
+
+
+Set.prototype.intersetion = function (otherset) {
+    const unionset = new Set();
+    for (let element of otherset) {
+        if (this.has(element)) {
+            unionset.add(element);
+        }
+    }
+    return unionset
+}
+console.log(setA.intersetion(setC));
+
+Set.prototype.difference = function (otherset) {
+    const unionset = new Set();
+    for (let element of otherset) {
+        if (!this.has(element)) {
+            unionset.add(element);
+        }
+    }
+    return unionset
+}
+
+console.log(setA.difference(setB));
+
+//blog seo keyword vigit
+class TagInput{
+    constructor(){
+        this.name="praveen" 
+        this.tag = new Set();
+    }
+    addInput(value){
+      this.tag.add(value)
+    }
+}
+ 
+const input =new TagInput();
+input.addInput(10)
+input.addInput(10)
+input.addInput(10)
+input.addInput(10)
+
+console.log(input);
+
+//--------------------------------------------------------------------------------------------------------------------------
